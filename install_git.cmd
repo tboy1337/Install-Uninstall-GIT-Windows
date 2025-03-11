a2996d16eb3a178d36290cc658d60f4ec5a85ac5
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-03-11 11:15:33 +0000

    Update install_git.cmd

--- install_git.cmd
+++ install_git.cmd
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Set variables for easy updating
-set "GIT_VERSION=2.47.1"
+set "GIT_VERSION=2.48.1"
 set "DOWNLOAD_URL=https://github.com/git-for-windows/git/releases/download/v%GIT_VERSION%.windows.1/Git-%GIT_VERSION%-64-bit.exe"
 set "INSTALLER_NAME=Git-%GIT_VERSION%-64-bit.exe"
 
